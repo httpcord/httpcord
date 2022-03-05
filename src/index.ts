cad0a12528e3c5f1cbc0b8ac4908b63102f41573
andre4ik3 <62390580+andre4ik3@users.noreply.github.com>
2022-03-05 19:47:59 +0400

    Whoops remove testing code

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,1 @@
-import { InteractionServer } from ".";
-
 export { InteractionServer } from "./InteractionServer";
-
-const server = new InteractionServer({ publicKey: "test" });
-
-server.slash.register(
-  {
-    name: "hello",
-    description: "gaming",
-    options: (opts) => ({
-      vim: opts.string("I don't know how to use vim"),
-    }),
-  },
-  async (i, { vim }) => {
-    console.log(vim);
-    await i.respond("gaming");
-  }
-);
