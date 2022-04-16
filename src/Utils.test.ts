f7557977762bcfbc1db6a4135d047670f12f8b55
andre4ik3 <62390580+andre4ik3@users.noreply.github.com>
2022-04-16 15:58:44 +0300

    Add utils test

---
+++ src/Utils.test.ts
@@ -0,0 +1,32 @@
+import { RequiresToken, sleep } from "./Utils";
+
+const marginOfError = 25;
+
+describe("Utils/Sleep", () => {
+  it("Sleeps for a second", async () => {
+    const time = performance.now();
+    await sleep(1000);
+    const time2 = performance.now();
+
+    expect(time2 - time).toBeGreaterThan(1000 - marginOfError);
+    expect(time2 - time).toBeLessThan(1000 + marginOfError);
+  });
+});
+
+describe("Utils/Decorators/RequiresToken", () => {
+  const testObject = { call: jest.fn(), api: { hasToken: false } };
+  const testDescriptor = Object.getOwnPropertyDescriptor(testObject, "call")!;
+
+  it("Throws when a token does not exist", () => {
+    const newDescriptor = RequiresToken(testObject, "call", testDescriptor);
+    expect(newDescriptor.value!).toThrow("call requires a bot token.");
+    expect(testObject.call.mock.calls).toHaveLength(0);
+  });
+
+  it("Does not throw when a token exists", () => {
+    testObject.api.hasToken = true;
+    const newDescriptor = RequiresToken(testObject, "call", testDescriptor);
+    newDescriptor.value!(); // Invoke the mock function
+    expect(testObject.call).toBeCalled();
+  });
+});
