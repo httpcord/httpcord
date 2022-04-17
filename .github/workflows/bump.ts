e8641c8b5d668e28ff6635460b3c8e71832e81ce
andre4ik3 <62390580+andre4ik3@users.noreply.github.com>
2022-04-17 16:57:53 +0300

    Dev publish tag

--- .github/workflows/bump.ts
+++
@@ -1,5 +0,0 @@
-const run = (...cmd: string[]) => Deno.run({ cmd }).status();
-const depMsg = "This old development version is no longer supported.";
-
-run("npm", "version", "prerelease", "--preid=dev", "--no-git-tag-version");
-run("npm", "deprecate", `httpcord@0.0.0-dev.x`, depMsg);
