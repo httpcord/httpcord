e9afc0d4786380aa61ccf9c70f646e004ed5d2e3
andre4ik3 <62390580+andre4ik3@users.noreply.github.com>
2022-04-23 00:27:16 +0300

    Role

--- src/Structures/Role.ts
+++ src/Structures/Role.ts
@@ -1,29 +1,86 @@
-import type { APIRole } from "../Types";
+import type { APIRole, Snowflake } from "../../Types";
+import type { ServerLike } from "../Base";
+import { Structure } from "../Base";
+import { Permissions } from "../Permissions";
+import type { AbstractGuild } from "./Abstract";
 
-export class Role {
-  id: string;
-  name: string;
-  color: number;
-  hoist: boolean;
-  position: number;
-  permissions: string;
-  managed: boolean;
-  mentionable: boolean;
+// This object does not have a "Abstract" counterpart because roles are always
+// received full. See "Resolved Data Structure" under "Receiving and Responding"
+// in the https://discord.dev/docs for more info.
 
-  icon?: string;
-  emoji?: string;
+interface RoleTags {
+  /** True if this is the guild's premium subscriber role. */
+  isPremiumSubscriberRole: boolean;
 
-  constructor(data: APIRole) {
-    this.id = data.id;
+  /** The ID of the integration that manages this role, if any. */
+  integrationId?: Snowflake;
+
+  /** The ID of the bot that manages this role, if any. */
+  botId?: Snowflake;
+}
+
+/** Represents a role received from Discord API. */
+export class Role extends Structure {
+  /** The guild that the role belongs to. */
+  public readonly guild: AbstractGuild;
+
+  /** The unique ID of this role (same as Guild ID if it's @everyone) */
+  public readonly id: Snowflake;
+
+  /** The name of the role. */
+  public readonly name: string;
+
+  /** The number representation of the hex color code of this role. */
+  public readonly color: number;
+
+  /** True if the guild is hoisted (displayed above others in member list). */
+  public readonly hoisted: boolean;
+
+  /** The position of the guild. */
+  public readonly position: number;
+
+  /** The permissions of this role. */
+  public readonly permissions: Permissions;
+
+  /** Whether or not this role is managed by an integration. */
+  public readonly managed: boolean;
+
+  /** Whether or not this role is mentionable. */
+  public readonly mentionable: boolean;
+
+  /** The hash of the role's icon, if any. */
+  public readonly icon?: string;
+
+  /** The emoji associated with the role, if any. */
+  public readonly emoji?: string;
+
+  /** The tags of the role, if any. */
+  public readonly tags?: RoleTags;
+
+  public constructor(server: ServerLike, guild: AbstractGuild, data: APIRole) {
+    super(server);
+
+    this.guild = guild;
+    this.id = data.id as Snowflake;
     this.name = data.name;
     this.color = data.color;
-    this.hoist = data.hoist;
+    this.hoisted = data.hoist;
     this.position = data.position;
-    this.permissions = data.permissions;
+    this.permissions = new Permissions(data.permissions);
     this.managed = data.managed;
     this.mentionable = data.mentionable;
 
     this.icon = data.icon || undefined;
     this.emoji = data.unicode_emoji || undefined;
+    this.tags = {
+      isPremiumSubscriberRole: data.tags?.premium_subscriber === null,
+      integrationId: data.tags?.integration_id as Snowflake,
+      botId: data.tags?.bot_id as Snowflake,
+    };
+  }
+
+  /** Returns the string representation of the role (the role as a mention). */
+  public toString() {
+    return `<@&${this.id}>`;
   }
 }
